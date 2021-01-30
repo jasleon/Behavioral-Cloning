8165f92dcc8a1c61fcb0fabcf19db55e91a984ed
jasleon <antonio.buap@gmail.com>
2021-01-30 10:31:29 -0600

    fix: Missing letters

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 
 lines = []
-with open('../data/driving_log.csv') as csvfile:
+with open('training/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
@@ -13,7 +13,7 @@ measurements = []
 for line in lines:
     source_path = line[0]
     filename = source_path.split('/')[-1]
-    current_path = '../data/IMG/' + filename
+    current_path = 'training/IMG/' + filename
     image = cv2.imread(current_path)
     images.append(image)
     measurement = float(line[3])
@@ -26,10 +26,10 @@ from keras.models import Sequential
 from keras.layers import Flatten, Dense
 
 model = Sequential()
-mode.add(Flatten(input_shape=(160, 320, 3)))
+model.add(Flatten(input_shape=(160, 320, 3)))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, suffle=True)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=8)
 
 model.save('model.h5')
